bd67f9d5e4d8b90032034e2c9519102362905be2
Brendon Butler <bbutler452@gmail.com>
2019-02-07 08:50:04 -0500

    Adding responses, initial question, indefinite loop, and breakpoint for loop

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Magic 8-Ball")
+responses = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.",
+             "You may rely on it.", "As I see it, yes.", "Outlook good", "Yes.",
+             "Signs point to yes.", "Reply hazy, try again.", "Ask again later.",
+             "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.",
+             "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
+
+print("Magic 8-Ball | What is your question?")
+
+while True:
+    question = input("\n> ")
+    if question.lower() == "quit" or question.lower() == "exit":
+        break
