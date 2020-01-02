0f2da2e7c28c351cb52fcf6b3480dca850058165
Brendon Butler <bbutler452@gmail.com>
2020-01-01 20:46:38 -0500

    Adding typical icosahedron responses and random response selector

--- Magic8Ball.cs
+++ Magic8Ball.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Magic8Ball
 {
     class Program
     {
+        private static Random rand = new Random();
+
         static void Main(string[] args)
         {
             do
@@ -11,7 +15,15 @@ namespace Magic8Ball
                 Console.Write("What is your question?\r\n> ");
                 string input = Console.ReadLine();
                 if (input.ToLower().Equals("exit")) break;
+                Console.WriteLine(GetResponse() + "\n");
             } while (true);
+        }
+
+        private static string GetResponse()
+        {
+            List<string> responses = new List<string>() { "It is certain", "It is decidedly so", "Without a doubt", "Yes - definitely", "You man rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful" };
+
+            return responses[rand.Next(responses.Count())];
         }
     }
 }
